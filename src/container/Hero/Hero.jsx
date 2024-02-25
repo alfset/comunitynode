cc3adcac3dceef2f4a4b9fd6f05f58fc605bf2f7
alfset <alfset5@gmail.com>
2024-02-25 18:39:17 +0700

    add supported network

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -15,42 +15,39 @@ const Hero = () => {
 
   return (
     <div className="hero">
-    <div className="row align-items-center">
-      <div className="col-md-6 col-12">
-        <h1 className="title fade-in">Stake, Earn, And Learn about Blockchain Project with us</h1>
+      <div className="row align-items-center">
+        <div className="col-md-6 col-12">
+          <h1 className="title fade-in">Stake, Earn, And Learn about Blockchain Project with us</h1>
+        </div>
+        <div className="col-md-6 col-12">
+          <Card className="green-card fade-in">
+            <Card.Body>
+              <Card.Title>Staking Stats</Card.Title>
+              <Card.Text><strong>Assets Staked:</strong> {stats.assetsStaked}</Card.Text>
+              <Card.Text><strong>Total Nodes Supported:</strong> {stats.totalNodes}</Card.Text>
+              <Card.Text><strong>Total Delegators:</strong> {stats.totalDelegators}</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
-      <div className="col-md-6 col-12">
-        <Card className="green-card fade-in">
-          <Card.Body>
-            <Card.Title>Staking Stats</Card.Title>
-            <Card.Text>
-              <strong>Assets Staked:</strong> {stats.assetsStaked}
-            </Card.Text>
-            <Card.Text>
-              <strong>Total Nodes Supported:</strong> {stats.totalNodes}
-            </Card.Text>
-            <Card.Text>
-              <strong>Total Delegators:</strong> {stats.totalDelegators}
-            </Card.Text>
-          </Card.Body>
-        </Card>
+
+      {/* Adding the Supported Network Heading */}
+      <div className="supported-network-title">Supported Network</div>
+
+      <div className="clients">
+        <div className="running-logos">
+          {logos.map((logo, index) => (
+            <img key={index} src={images[logo]} alt="logo" />
+          ))}
+          {/* Duplicate logos for a seamless loop */}
+          {logos.map((logo, index) => (
+            <img key={`duplicate-${index}`} src={images[logo]} alt="logo" />
+          ))}
+        </div>
       </div>
+      
+      <IconScroll />
     </div>
-  
-    {/* Updated part */}
-    <div className="clients">
-      <div className="running-logos">
-        {logos.map((logo, index) => (
-          <img key={index} src={images[logo]} alt="logo" />
-        ))}
-        {/* Duplicate logos for a seamless loop */}
-        {logos.map((logo, index) => (
-          <img key={`duplicate-${index}`} src={images[logo]} alt="logo" />
-        ))}
-      </div>
-    </div>
-    <IconScroll />
-  </div>
   );
 }
 
