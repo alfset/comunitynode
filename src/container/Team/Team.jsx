cb6ad819392ebee86e99ddaf1fef31114b5ff293
Alf <42708476+alfset@users.noreply.github.com>
2024-11-30 02:18:27 +0700

    Update Team.jsx

--- src/container/Team/Team.jsx
+++ src/container/Team/Team.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Headings } from '../../components';
 import { data } from '../../constants';
 import { BsLinkedin } from 'react-icons/bs';
@@ -7,25 +7,31 @@ import './Team.css';
 const Team = () => {
   return (
     <div className="section-padding">
-      <Headings title="Team" text="Meet the skilled and experienced team behind our Blockchain Journey" />
-
+      <Headings title="Team" text="Our skilled and experienced team behind our Blockchain Journey" />
+      
       <div className="row">
-        {data.Team.map(({ name, position, info, linkedin }, index) => (
-          <div className="col-lg-4 col-md-6 col-12">
-            <div className="card-team" key={index}>
+        {data.Team.map(({ name, position, info, linkedin, image }, index) => (
+          <div className="col-lg-4 col-md-6 col-12" key={index}>
+            <div className="card-team">
               <div className="card-team-header">
-                <p> <span>{name}</span>
-                  {position}</p>
-                <a href={linkedin} alt={`Linkedin + ${name}`} ><BsLinkedin /></a>
+                <img src={image} alt={name} className="team-image" />
+                <div>
+                  <p>
+                    <span>{name}</span>
+                    {position}
+                  </p>
+                </div>
+                <a href={linkedin} target="_blank" rel="noopener noreferrer" aria-label={`LinkedIn ${name}`}>
+                  <BsLinkedin />
+                </a>
               </div>
               <p>{info}</p>
             </div>
           </div>
         ))}
-
       </div>
     </div>
-  )
+  );
 }
 
-export default Team
+export default Team;
